d92681091c496812fb8e5716f51f90871db06ed0
Hecatomb <60758685+pedroelbanquero@users.noreply.github.com>
2024-05-04 21:11:06 +0200

    Update torah.py

--- torahcodes/resources/func/torah.py
+++ torahcodes/resources/func/torah.py
@@ -70,7 +70,7 @@ class Torah():
 		translated = GoogleTranslator(source=lang_in, target=lang_out).translate(data.strip()) 
 		return translated
 		
-	def gematria_sum(palabra):
+	def gematria_sum(self,palabra):
 		
 	    valores = {'א': 1, 'ב': 2, 'ג': 3, 'ד': 4, 'ה': 5, 'ו': 6, 'ז': 7, 'ח': 8, 'ט': 9,
 	           'י': 10, 'כ': 20, 'ל': 30, 'מ': 40, 'נ': 50, 'ס': 60, 'ע': 70, 'פ': 80,
